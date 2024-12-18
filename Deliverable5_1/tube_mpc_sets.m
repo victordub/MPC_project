eedab9a79773ae5a435b2b200e9e8becc989027f
Victor Dubois <victor.dubois@outlook.com>
2024-12-18 18:56:48 +0100

    part 5 setup

---
+++ Deliverable5_1/tube_mpc_sets.m
@@ -0,0 +1,76 @@
+addpath('../car_project/')
+
+Ts = 1/10;
+
+car = Car(Ts);
+[xs, us] = car.steady_state(120 / 3.6);
+uTs = us(2);
+sys = car.linearize(xs, us);
+[sys_lon, ~] = car.decompose(sys);
+
+sysl_lond = c2d(sys_lon, Ts);
+[A, B, C, D] = ssdata(sysl_lond);
+
+[nx, nu] = size(B);
+
+Q = 1*eye(nx);
+R = 10;
+
+[K,Qf,~] = dlqr(A,-B,Q,R);
+K = -K;
+
+%% Minimal invariant set
+u_tilde = Polyhedron([1; -1], [uTs + 0.5; - uTs + 0.5]);
+W = B*u_tilde;
+
+% Compute first step
+omega = (A-B*K)^0*W;
+
+% Loop
+k = 1;
+while 1
+    AkW = (A-B*K)^k*W;
+    omega_new = plus(omega,AkW);
+    if norm((A-B*K)^k) < 1e-2
+        F_inf = omega;
+        break
+    end
+    k = k+1;
+    omega_new.minHRep;
+    omega = omega_new;
+end
+
+%% Tightened Constraints
+x_safe = 10;
+
+H = [-1  0];
+h = [-6+x_safe];
+
+% X
+X = Polyhedron(H,h);
+
+X_tilde = minus(X, F_inf);
+
+% U
+U = Polyhedron([1; -1], [1; 1]);
+
+U_tilde = minus(U, K*F_inf);
+
+%% Terminal Components
+F = X_tilde.A;
+f = X_tilde.b;
+M = U_tilde.A;
+m = U_tilde.b;
+Xf = polytope([F;M*K],[f;m]);
+Acl = [A-B*K];
+while 1
+    prevXf = Xf;
+    [T,t] = double(Xf);
+    preXf = polytope(T*Acl,t);
+    Xf = intersect(Xf, preXf);
+    if isequal(prevXf, Xf)
+        break
+    end
+end
+
+save('tube_mpc_data.mat', 'F_inf', 'Xf', 'Qf', 'K', 'X_tilde', 'U_tilde');
